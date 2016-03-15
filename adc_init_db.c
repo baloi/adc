06bb259aca62070ae0dbc8948da8bbed226d29da
drogos <drogos@drogo.net>
2016-03-14 21:35:43 -0400

    Mondays

--- adc_init_db.c
+++ adc_init_db.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <sqlite3.h>
 
+static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+    int i;
+    for(i = 0; i < argc; i++) {
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+    printf("\n");
+    return 0;
+}
+
 int main(int argc, char **argv) {
     sqlite3 *db;
     char *zErrMsg = 0;
     int rc;
+    char *stmt = "CREATE TABLE IF NOT EXISTS residents(id text, lastname text, firstname text, schedule text);";
 
     if (argc != 2) {
         fprintf(stderr, "Usage: %s DATABASE\n", argv[0]);
@@ -18,6 +28,8 @@ int main(int argc, char **argv) {
         sqlite3_close(db);
         return(1);
     }
+    
+    rc = sqlite3_exec(db, stmt, callback, 0, & zErrMsg);
 
     if (rc != SQLITE_OK) {
         fprintf(stderr, "SQL Error: %s\n", zErrMsg);
