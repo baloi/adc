7fee8a8b553680ba8ff8495551a0a33c5d6c1b61
drogos <drogos@drogo.net>
2016-03-14 22:51:03 -0400

    running with malloc

---
+++ formatting_test.c
@@ -0,0 +1,21 @@
+/* 
+ * Testing sprintf formatting....
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char *a_name = "The Name";
+    char *format_string = "This is %s\n";
+    long str_size = sizeof(a_name) + sizeof(format_string);
+    char *full_string = malloc(str_size);
+    //char full_string[255];
+
+    //printf("size of string = %d\n", str_size);
+
+    sprintf(full_string, format_string, a_name);
+
+    printf(full_string);
+
+    return 0;
+}
